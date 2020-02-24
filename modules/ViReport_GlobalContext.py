fe67cbc269c467fd7044fc625d2cb4a2b774c69b
Niema Moshiri <niemamoshiri@gmail.com>
2020-02-24 11:06:11 -0800

    Added Global Context

---
+++ modules/ViReport_GlobalContext.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+'''
+Store global variables/functions to be accessible by all ViReport modules
+'''
+
+# useful constants
+CITATION_VIREPORT = 'Moshiri N. (2020). "ViReport" (https://github.com/niemasd/ViReport).'
