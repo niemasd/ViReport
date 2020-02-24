5ee49ba898f3afafe3780212a3c93f5b4d6ddba6
Niema Moshiri <niemamoshiri@gmail.com>
2020-02-24 09:44:46 -0800

    Added MSA module

---
+++ modules/MultipleSequenceAlignment.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+'''
+"MultipleSequenceAlignment" module
+'''
+import abc
+
+class MultipleSequenceAlignment(metaclass=abc.ABCMeta):
+    '''
+    Abstract class defining the ``MultipleSequenceAlignment`` module
+
+    Methods
+    -------
+    align(seqs_filename)
+        Align the sequences in ``seqs_filename``
+    cite()
+        Return citation string (or None)
+    init()
+        Initialize the module (if need be)
+    finalize()
+        Finalize the module (if need be)
+    '''
+
+    @staticmethod
+    @abc.abstractmethod
+    def init():
+        '''
+        Initialize the module (if need be)
+        '''
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def finalize():
+        '''
+        Finalize the module (if need be)
+        '''
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def cite():
+        '''
+        Return the citation string (or None)
+        '''
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def align(seqs_filename):
+        '''
+        Align the sequences in ``seqs_filename``
+
+        Parameters
+        ----------
+        seqs_filename : str
+            Filename of the raw sequences to align (in the FASTA format)
+        '''
+        pass
